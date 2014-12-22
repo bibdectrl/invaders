3d1194589f68a5aaf8492de2de6484d84451a5ac
bibdectrl <bibdectrl@github>
2014-12-22 18:55:14 -0500

    shooting sort of works

--- invaders.py
+++ invaders.py
@@ -2,8 +2,8 @@ import pygame
 from pygame.locals import K_LEFT, K_RIGHT, K_SPACE, K_ESCAPE, KEYDOWN, KEYUP
 from random import randint
 
-WIDTH = 64
-HEIGHT = 48
+WIDTH = 640
+HEIGHT = 480
 
 class Game:
     def __init__(self):
@@ -14,7 +14,7 @@ class Game:
         self.game_over = False
 
     def make_aliens(self):
-        return [Alien(self, x*3, y*3) for x in range(10) for y in range(6)]
+        return [Alien(self, x*30, y*30) for x in range(10) for y in range(6)]
 
     def set_alien_direction(self):
         # aliens have hit the right side of the screen, so they switch directions to move down
@@ -38,28 +38,29 @@ class Game:
         self.detect_collisions()
         for alien in self.aliens:
             alien.move()
+            alien.shoot_maybe()
         for bullet in self.alien_bullets:
             bullet.move()
         for bullet in self.player_bullets:
             bullet.move()
-#        self.alien_bullets = filter(lambda b : b < HEIGHT and b >= 0, self.alien_bullets)
+        self.alien_bullets = filter(lambda b : b.y < 480, self.alien_bullets)
         self.player_bullets = filter(lambda b : b.y >= 0, self.player_bullets)
         self.set_alien_direction()
 
     def detect_collisions(self):
         for bullet in self.player_bullets:
             for alien in self.aliens:
-                if bullet.x == alien.x and bullet.y == alien.y:
+                if abs(bullet.x - alien.x) < 10 and abs(bullet.y - alien.y) < 10:
                     alien.alive = False
         self.aliens = filter(lambda alien : alien.alive, self.aliens)
 
         for bullet in self.alien_bullets:
-            if bullet.x == player.x and bullet.y == player.y:
+            if bullet.x == self.player.x and bullet.y == self.player.y:
                 self.player.lives -= 1
-                if player.lives < 0:
+                if self.player.lives < 0:
                     self.game_over = True
                 else:
-                    player.x = WIDTH / 2
+                    self.player.x = WIDTH / 2
 
 class Alien:
     def __init__(self, game, x, y):
@@ -72,8 +73,8 @@ class Alien:
         self.rect = self.img.get_rect()
 
     def shoot_maybe(self):
-        if randint(10) == 1:
-            game.bullets.append(Bullet(self.x, self.y, 1))
+        if randint(0, 1000) == 1:
+            self.game.alien_bullets.append(Bullet(self.x, self.y, 1))
 
     def __str__(self):
         return "Alien at " + str(self.x) + ", " + str(self.y)
@@ -92,7 +93,7 @@ class Alien:
 class Player:
     def __init__(self, game):
         self.x = WIDTH / 2
-        self.y = 460
+        self.y = 440
         self.game = game
         self.img = pygame.image.load("player.png")
         self.rect = self.img.get_rect()
@@ -103,9 +104,15 @@ class Player:
             self.speed = 4
         elif direction == -1:
             self.speed = -4
+        else:
+            self.speed = 0
 
     def move(self):
         self.x += self.speed
+        if self.x < 10:
+            self.x = 10
+        elif self.x > 610:
+            self.x = 610
 
     def shoot(self):
         if len(self.game.player_bullets) < 5:
@@ -127,9 +134,9 @@ class Bullet:
 
     def move(self):
         if self.direction == 1:
-            self.y += 10
+            self.y += 20
         else:
-            self.y -= 10
+            self.y -= 20
 
 def main():
     pygame.init()
@@ -163,13 +170,13 @@ def main():
 
             main_surface.fill((0, 0, 0, 0))
             for alien in game.aliens:
-                main_surface.blit(alien.img, (alien.x * 10, alien.y * 10))
+                main_surface.blit(alien.img, (alien.x, alien.y))
             for bullet in game.player_bullets:
                 main_surface.blit(bullet.img, (bullet.x, bullet.y))
             for bullet in game.alien_bullets:
-                main_surface.blit(bullet.img, (bullet.x * 10, bullet.y * 10))
+                main_surface.blit(bullet.img, (bullet.x, bullet.y))
 
-            main_surface.blit(game.player.img, (game.player.x, 460))
+            main_surface.blit(game.player.img, (game.player.x, game.player.y))
             pygame.display.flip()
 
             game.player.move()
